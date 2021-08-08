adb171a8f05d17bef2f7c3df843eb2829100adf2
vp275 <41702642+vp275@users.noreply.github.com>
2021-08-08 12:02:29 +0530

    minor changes

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -24,8 +24,8 @@
 
 ;; (setq doom-font (font-spec :family "Sarasa Term K" :size 25))
 ;; (setq doom-variable-pitch-font (font-spec :family "Sarasa Term K" :size 25))
-(setq doom-font (font-spec :family "Iosevka" :size 25 :weight 'Regular))
-(setq doom-variable-pitch-font (font-spec :family "Iosevka" :size 25 :weight 'Light))
+(setq doom-font (font-spec :family "JetBrains Mono" :size 25 :weight 'Light))
+(setq doom-variable-pitch-font (font-spec :family "JetBrains Mono" :size 25 :weight 'Light))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
